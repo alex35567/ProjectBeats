642f9f0de57aaa46d536d81b13e3a9b278304c34
Alex35567 <Alex35567@rambler.ru>
2022-12-03 17:31:32 +0500

    Поравил кнопку в слайдере, в аккардеоне открыл последнюю карточку

--- scripts/slider.js
+++ scripts/slider.js
@@ -1,22 +1,25 @@
-$(document).ready(function(){
-    
-    var slider = $('.slider').bxSlider({
-        wrapperClass:'products-section',
-        pager:false,
-        controls:false,        
-    })
+$(document).ready(function () {
 
-    $('.products__arrow-prev').click(function(e){
-        e.preventDefault();
-        slider.goToNextSlide();
-        return false;
-      });
-  
-    $('.products__arrow-next').click(function(e){
-        e.preventDefault();
-        slider.goToPrevSlide();
-        return false;
-      });
+  let e_command = new Event("click");
+  document.querySelector('.command__elem:last-child').dispatchEvent(e_command);
+
+  var slider = $('.slider').bxSlider({
+    wrapperClass: 'products-section',
+    pager: false,
+    controls: false,
+  })
+
+  $('.products__arrow-prev').click(function (e) {
+    e.preventDefault();
+    slider.goToNextSlide();
+    return false;
+  });
+
+  $('.products__arrow-next').click(function (e) {
+    e.preventDefault();
+    slider.goToPrevSlide();
+    return false;
+  });
 
 
 });
