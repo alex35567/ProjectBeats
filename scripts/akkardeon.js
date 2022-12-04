46df4ac73a8069b5c4f0f5678c1e4b59c8c09211
Alex35567 <Alex35567@rambler.ru>
2022-12-04 15:29:43 +0500

    Плавное изменение изображения tablets

--- scripts/akkardeon.js
+++ scripts/akkardeon.js
@@ -1,14 +1,15 @@
-
-$('.command__elem').on('click',function(e){
+$('.command__elem').on('click', function (e) {
     e.preventDefault();
-    $(this).toggleClass('active'); 
+    $(this).toggleClass('active');
 
     let job_title = $(this).find('.command__job-title');
     let desc = $(this).find('.command__desc');
-    
-    let image =$(this).find('.command__image-tablets');
+
+    let image = $(this).find('.command__image-tablets');
+    // imageHeight = image.outerHeight(true);
+
     let imageHeight = 0;
-    if (image.css('display')!='none'){
+    if (image.css('display') != 'none') {
         imageHeight = image.outerHeight(true);
     }
 
@@ -16,14 +17,14 @@ $('.command__elem').on('click',function(e){
     let h_cart = $(this).find('.command__cart');
 
     //toggleCss
-    if (h_cart.height()>0) {h_cart.height(0);}
-    else {h_cart.height(job_title.outerHeight(true)+desc.outerHeight(true)+imageHeight)};
+    if (h_cart.height() > 0) { h_cart.height(0); }
+    else { h_cart.height(job_title.outerHeight(true) + desc.outerHeight(true) + imageHeight) };
 
     //удаляем класс с соседних элементов
-    $('.command__elem').each((ind, item)=>{
-        if(item!=this){
+    $('.command__elem').each((ind, item) => {
+        if (item != this) {
             $(item).removeClass('active');
             $(item).find('.command__cart').height(0);
-        } 
+        }
     });
 });
