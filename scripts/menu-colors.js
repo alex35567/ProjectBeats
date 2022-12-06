9d7b0aa4e9fbeb24471ae7870b1c443e5e5bd11e
Alex35567 <Alex35567@rambler.ru>
2022-12-06 21:01:31 +0500

    гориз аккардеон планшеты-телефоны

--- scripts/menu-colors.js
+++ scripts/menu-colors.js
@@ -1,7 +1,7 @@
 $('.colors-section').on('click', function (e) {
     e.preventDefault();
     if (this == e.target) {
-        if ($('.colors__cart--active').length>0) {
+        if ($('.colors__cart--active').length > 0) {
             $('.colors-title').toggleClass('colors-title--active');
         }
         $('.colors__cart--active').removeClass('colors__cart--active');
@@ -15,12 +15,19 @@ $('.colors__elem-title').on('click', function (e) {
 
     const cart = $(this).siblings('.colors__cart');
     const parent = cart.closest('.colors__elem');
+    //закрываем все открытые карточки, кроме выбранной    
     parent.siblings().find('.colors__cart--active').removeClass('colors__cart--active');
 
     cart.toggleClass('colors__cart--active');
 
     $('.colors-title').removeClass('colors-title--active');
-    $('#menu').addClass('colors-title--active');
+    $('.colors-title-menu').addClass('colors-title--active');
+
+    if ($(window).width() <= 480) {        
+        parent.toggleClass('colors__elem--active');
+        // cart.find('.colors__desc').width($(window).width()-171);
+        cart.find('.colors__desc').css('width',($(window).width()-90+'px'));            
+    }
 
     if ($('.colors__cart--active').length == 0) {
         $('.colors-title').toggleClass('colors-title--active');
--- scripts/slider.js
+++ scripts/slider.js
@@ -3,10 +3,16 @@ $(document).ready(function () {
 
   $(window).resize(function(e){
 
+    //закрываем вертикальный аккардеон при смене размера окна    
     const comElem = $('.command__elem.active');
     comElem.removeClass('active');
     const h_cart = comElem.find('.command__cart');
     h_cart.height(0); 
+
+    //закрываем меню colors (горизонтальный аккардеон) при изменении размера окна
+    $('.colors__cart--active').removeClass('colors__cart--active');    
+    $('.colors__elem--active').removeClass('colors__elem--active');
+    $('.colors__desc').css('width','');  
 
   })
 
